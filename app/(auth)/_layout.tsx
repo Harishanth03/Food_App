a29b5c8d11ab7428ea4fca6a7e7aee0f64bd0fb7
Harishanth03 <kandeepanharishanth@gmail.com>
2025-08-24 20:50:44 +0530

    Last Auth Changes

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,13 +1,19 @@
-import {View, Text} from 'react-native'
+import {KeyboardAvoidingView, Platform, ScrollView, View, Dimensions, ImageBackground, Image} from 'react-native'
 import React from 'react'
 import {Slot} from "expo-router";
-import {SafeAreaView} from "react-native-safe-area-context";
+import {images} from "@/constants";
 
 export default function _Layout() {
     return (
-        <SafeAreaView>
-            <Text>Auth_Layout</Text>
+        <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+            <ScrollView className="bg-white h-full" keyboardShouldPersistTaps="handled">
+                <View className="w-full relative" style={{height: Dimensions.get('screen').height / 2.25}}>
+                    <ImageBackground source={images.loginGraphic} className="size-full rounded-b-lg" resizeMode={"stretch"}/>
+                    <Image source={images.logo} className="absolute -bottom-16 z-10 self-center size-48"/>
+                </View>
+
+            </ScrollView>
             <Slot/>
-        </SafeAreaView>
+        </KeyboardAvoidingView>
     )
 }
