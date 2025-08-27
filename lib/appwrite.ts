e24d4f408bd50b28866b2de2a9431cf44f8d161c
Harishanth03 <kandeepanharishanth@gmail.com>
2025-08-27 11:59:41 +0530

    Add User code

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,7 +1,60 @@
+import {Account, Avatars, Client, Databases, ID} from "react-native-appwrite";
+import {CreateUserParams, SignInParams} from "@/type";
+import {id} from "postcss-selector-parser";
+
 export const appwriteConfig = {
-    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT!,
     platform: "com.harishanth.fastfood",
-    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!,
     databaseId: "68ab218d00347e5589bd",
     userCollectionId: "68ab21c6003ab54c6d20"
+}
+
+export const client = new Client();
+
+client
+    .setEndpoint(appwriteConfig.endpoint)
+    .setProject(appwriteConfig.projectId)
+    .setPlatform(appwriteConfig.platform)
+
+export const account = new Account(client);
+export const database = new Databases(client)
+export const avatar = new Avatars(client)
+
+export const createUser = async ({email,password,name}: CreateUserParams) => {
+    try {
+        const newAccount = await account.create(ID.unique(), email, password, name)
+
+        if(!newAccount) throw Error(`Account with id ${id} already exists`)
+
+        await signIn({email, password})
+
+        const avartarUrl = avatar.getInitialsURL(name)
+
+        return await database.createDocument(
+            appwriteConfig.databaseId,
+            appwriteConfig.userCollectionId,
+            ID.unique(),
+            {
+                accountId: newAccount.$id,
+                email,name,avatar:avartarUrl
+            }
+        )
+
+    }
+    catch (error) {
+        throw new Error(error as string)
+    }
+
+}
+
+export const signIn = async ({email, password} : SignInParams) => {
+    try {
+        const session = await account.createEmailPasswordSession(email, password)
+
+    }
+    catch (error) {
+        throw new Error(error as string)
+    }
+
 }
